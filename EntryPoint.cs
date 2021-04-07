cd6ded23943336f1f41f8061a531527585035b29
Hamish McKenzie <hamish.mckenzie@protonmail.com>
2021-04-07 11:13:43 +1000

    Added some comments to clarify what is happening

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -18,23 +18,23 @@ namespace IredesChecksum
             var retVal = 0;
             foreach (var filepath in args)
             {
-                var (stored, computed) = GetChecksum(args[0]);
+                var (stored, computed) = _getChecksum(args[0]);
                 Console.WriteLine($"Checking \"{filepath}\"");
                 Console.WriteLine($"Stored checkSum is:    {stored}");
                 Console.WriteLine($"Computed checkSum is:  {computed}");
                 if (computed != stored)
                 {
-                    Console.WriteLine($"***ERROR - checksum mismatch***");
+                    Console.WriteLine("***ERROR - checksum mismatch***");
                     retVal = 1;
                 }
                 else
-                    Console.WriteLine($"SUCCESS - checksum is correct");
+                    Console.WriteLine("SUCCESS - checksum is correct");
             }
 
             return retVal;
         }
 
-        static (uint Stored, uint Computed) GetChecksum(string filepath)
+        static (uint Stored, uint Computed) _getChecksum(string filepath)
         {
             const string checkSumTag = "<IR:ChkSum>";
             const string endTag = "</";
@@ -42,26 +42,35 @@ namespace IredesChecksum
             using (var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
                 contentsStr = fileStream.ReadToString();
 
+            // Look for the start of the checksum node
             var indexStart = contentsStr.IndexOf(checkSumTag);
             if (indexStart < 0)
                 indexStart = 0;
 
+            // Add the length of the string to the start idx
             indexStart += checkSumTag.Length;
+
+            // Find the start of the end of the checksum tag so we know where to
+            // slice the string
             var indexEnd = contentsStr.IndexOf(endTag, indexStart);
             if (indexEnd < 0)
                 indexEnd = indexStart;
 
+            // If there is no checksum found, consider it to be zero...
             var checkSumStr = indexStart == indexEnd ?
                 "0" :
-                contentsStr.Substring(indexStart, indexEnd - indexStart);
+                contentsStr[indexStart..indexEnd];
             var storedCheckSum = uint.Parse(checkSumStr);
 
+            // Now copy the contents of the file to a byte array but replace the stored
+            // checksum value with 0
             var encoding = Encoding.UTF8;
             var newContentsBytes = new byte[contentsStr.Length - (indexEnd - indexStart) + 1];
-            encoding.GetBytes(contentsStr.Substring(0, indexStart)).CopyTo(newContentsBytes, 0);
+            encoding.GetBytes(contentsStr[..indexStart]).CopyTo(newContentsBytes, 0);
             encoding.GetBytes("0").CopyTo(newContentsBytes, indexStart);
-            encoding.GetBytes(contentsStr.Substring(indexEnd)).CopyTo(newContentsBytes, indexStart + 1);
+            encoding.GetBytes(contentsStr[indexEnd..]).CopyTo(newContentsBytes, indexStart + 1);
 
+            // Return the stored and computed checksum values
             return (storedCheckSum, Crc32.Compute(newContentsBytes));
         }
     }
